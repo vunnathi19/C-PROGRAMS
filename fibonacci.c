c270543cb3947e850198346a78e72873e406315d
GUTTA.VUNNATHI <vunnathichowdary@gmail.com>
2026-05-19 16:13:25 +0530

    Added fibonacci series program

---
+++ fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+
+    int n, first = 0, second = 1, next, i;
+
+    printf("Enter number of terms: ");
+    scanf("%d", &n);
+
+    printf("Fibonacci Series:\n");
+
+    for(i = 0; i < n; i++) {
+
+        printf("%d ", first);
+
+        next = first + second;
+        first = second;
+        second = next;
+    }
+
+    return 0;
+}
